07cb8558b5062cc9fdbc6604c9eb04245a38137f
Ahmadhcs <aalmesned1.sch@gmail.com>
2024-04-08 18:43:19 -0400

    Fixing app router

--- Rental/src/App.js
+++ Rental/src/App.js
@@ -1,6 +1,14 @@
-import React from "react";
-import AppRouter from "./AppRouter";
-
-export default function App() {
-  return <AppRouter />;
+import React from 'react';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import LandingPage from './Screens/LandingPage';
+function App() {
+  return (
+    <Router>
+      <Routes>
+        <Route path="/" element={<LandingPage />} />
+      </Routes>
+    </Router>
+  );
 }
+
+export default App;
--- Rental/src/Screens/LandingPage.jsx
+++ Rental/src/Screens/LandingPage.jsx
@@ -1,5 +1,12 @@
  const LandingPage = () =>{
-    return 
+    console.log("renders")
+    return (
+        <div>
+            <h1>
+                hello
+            </h1>
+        </div>
+    )
 }
 
 export default LandingPage
